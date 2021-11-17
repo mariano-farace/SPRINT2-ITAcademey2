09ad7d5ee3905b83e8d5a3b3ffbb6e170ac5f8bd
Mariano <mariano_farace@hotmail.com>
2021-11-17 09:05:18 +0100

    Remove ESLint

---
+++ mySQL-estructura/optica-sql.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE optica
+USE optica
+
+CREATE TABLE glasses 
+(brand VARCHAR(20), 
+graduation_r_lens VARCHAR(20),
+graduation_l_lens VARCHAR(20),
+frame_type VARCHAR(20),
+frame_color VARCHAR(20),
+lens_color VARCHAR(20),
+price VARCHAR(20),
+stock VARCHAR(20),
+glassesSupplier_Id 
+
+DESCRIBE pet
